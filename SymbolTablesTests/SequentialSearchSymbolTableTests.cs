b1116df3fef40cfe0dfba1e5f7329263aab38722
Joseph Akroush <JosephAkroush@users.noreply.github.com>
2017-07-19 23:43:24 -0400

    formatting

--- SymbolTablesTests/SequentialSearchSymbolTableTests.cs
+++ SymbolTablesTests/SequentialSearchSymbolTableTests.cs
@@ -27,8 +27,8 @@ namespace SymbolTablesTests
         [Fact]
         public void AddingItemsShouldReflectCorrectSize()
         {
-			// Arrange
-			var target = new SequentialSearchSymbolTable<SocialSecurityNumber, Person>();
+            // Arrange
+            var target = new SequentialSearchSymbolTable<SocialSecurityNumber, Person>();
 
             SocialSecurityNumber ssn1 = new SocialSecurityNumber("555", "22", "3333");
             SocialSecurityNumber ssn2 = new SocialSecurityNumber("111", "33", "4444");
@@ -44,66 +44,66 @@ namespace SymbolTablesTests
 
             int actual = target.Size();
 
-			// Assert
-			Assert.Equal(expected, actual);
+            // Assert
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
         public void UpdatingItemShouldNotReturnDifferentSize()
-		{
-			// Arrange
-			var target = new SequentialSearchSymbolTable<SocialSecurityNumber, Person>();
+        {
+            // Arrange
+            var target = new SequentialSearchSymbolTable<SocialSecurityNumber, Person>();
 
-			SocialSecurityNumber ssn1 = new SocialSecurityNumber("555", "22", "3333");
-			SocialSecurityNumber ssn2 = new SocialSecurityNumber("111", "33", "4444");
+            SocialSecurityNumber ssn1 = new SocialSecurityNumber("555", "22", "3333");
+            SocialSecurityNumber ssn2 = new SocialSecurityNumber("111", "33", "4444");
 
-			Person person1 = new Person("John", "Doe");
-			Person person2 = new Person("Jane", "Smith");
+            Person person1 = new Person("John", "Doe");
+            Person person2 = new Person("Jane", "Smith");
             Person person3 = new Person("Tom", "Cruise");
 
-			int expected = 2;
+            int expected = 2;
 
-			// Act
+            // Act
 
             // Insert new items
-			target.Put(ssn1, person1);
-			target.Put(ssn2, person2);
+            target.Put(ssn1, person1);
+            target.Put(ssn2, person2);
 
             // Update ssn2 with a new person.
             target.Put(ssn2, person3);
 
-			int actual = target.Size();
+            int actual = target.Size();
 
-			// Assert
-			Assert.Equal(expected, actual);
-		}
+            // Assert
+            Assert.Equal(expected, actual);
+        }
 
         [Fact]
         public void InsertedItemKeysShouldBeReturnedWhenKeysIsCalled()
         {
-			// Arrange
-			var target = new SequentialSearchSymbolTable<SocialSecurityNumber, Person>();
+            // Arrange
+            var target = new SequentialSearchSymbolTable<SocialSecurityNumber, Person>();
 
-			SocialSecurityNumber ssn1 = new SocialSecurityNumber("555", "22", "3333");
-			SocialSecurityNumber ssn2 = new SocialSecurityNumber("111", "33", "4444");
+            SocialSecurityNumber ssn1 = new SocialSecurityNumber("555", "22", "3333");
+            SocialSecurityNumber ssn2 = new SocialSecurityNumber("111", "33", "4444");
             SocialSecurityNumber ssn3 = new SocialSecurityNumber("456", "12", "7890");
 
-			Person person1 = new Person("John", "Doe");
-			Person person2 = new Person("Jane", "Smith");
-			Person person3 = new Person("Tom", "Cruise");
+            Person person1 = new Person("John", "Doe");
+            Person person2 = new Person("Jane", "Smith");
+            Person person3 = new Person("Tom", "Cruise");
 
             IEnumerable<SocialSecurityNumber> expected = new[] { ssn1, ssn2, ssn3 };
 
-			// Act
+            // Act
 
-			// Insert new items
-			target.Put(ssn1, person1);
-			target.Put(ssn2, person2);
+            // Insert new items
+            target.Put(ssn1, person1);
+            target.Put(ssn2, person2);
             target.Put(ssn3, person3);
 
             IEnumerable<SocialSecurityNumber> actual = target.Keys();
 
-			// Assert
+            // Assert
             Assert.Equal(expected.Count(), actual.Count());
             Assert.True(actual.Where(x => x.SerialNumber.Equals("3333")).Count() == 1);
             Assert.True(actual.Where(x => x.SerialNumber.Equals("4444")).Count() == 1);
@@ -113,26 +113,26 @@ namespace SymbolTablesTests
         [Fact]
         public void DeleteShouldReflectInKeysAndSize()
         {
-			// Arrange
-			var target = new SequentialSearchSymbolTable<SocialSecurityNumber, Person>();
+            // Arrange
+            var target = new SequentialSearchSymbolTable<SocialSecurityNumber, Person>();
 
-			SocialSecurityNumber ssn1 = new SocialSecurityNumber("555", "22", "3333");
-			SocialSecurityNumber ssn2 = new SocialSecurityNumber("111", "33", "4444");
-			SocialSecurityNumber ssn3 = new SocialSecurityNumber("456", "12", "7890");
+            SocialSecurityNumber ssn1 = new SocialSecurityNumber("555", "22", "3333");
+            SocialSecurityNumber ssn2 = new SocialSecurityNumber("111", "33", "4444");
+            SocialSecurityNumber ssn3 = new SocialSecurityNumber("456", "12", "7890");
 
-			Person person1 = new Person("John", "Doe");
-			Person person2 = new Person("Jane", "Smith");
-			Person person3 = new Person("Tom", "Cruise");
+            Person person1 = new Person("John", "Doe");
+            Person person2 = new Person("Jane", "Smith");
+            Person person3 = new Person("Tom", "Cruise");
 
             IEnumerable<SocialSecurityNumber> expectedKeys = new[] { ssn2 };
             int expectedSize = 0;
 
-			// Act
+            // Act
 
-			// Insert new items
-			target.Put(ssn1, person1);
-			target.Put(ssn2, person2);
-			target.Put(ssn3, person3);
+            // Insert new items
+            target.Put(ssn1, person1);
+            target.Put(ssn2, person2);
+            target.Put(ssn3, person3);
 
             target.Delete(ssn3);
             target.Delete(ssn1);
@@ -141,11 +141,11 @@ namespace SymbolTablesTests
             IEnumerable<SocialSecurityNumber> actualKeys = target.Keys();
             int actualSize = target.Size();
 
-			// Assert
-			//Assert.Equal(expectedKeys.Count(), actualKeys.Count());
-			//Assert.True(actualKeys.Where(x => x.SerialNumber.Equals("3333")).Count() == 0);
-			//Assert.True(actualKeys.Where(x => x.SerialNumber.Equals("4444")).Count() == 1);
-			//Assert.True(actualKeys.Where(x => x.SerialNumber.Equals("7890")).Count() == 0);
+            // Assert
+            //Assert.Equal(expectedKeys.Count(), actualKeys.Count());
+            //Assert.True(actualKeys.Where(x => x.SerialNumber.Equals("3333")).Count() == 0);
+            //Assert.True(actualKeys.Where(x => x.SerialNumber.Equals("4444")).Count() == 1);
+            //Assert.True(actualKeys.Where(x => x.SerialNumber.Equals("7890")).Count() == 0);
             Assert.Null(actualKeys);
             Assert.Equal(expectedSize, actualSize);
         }
